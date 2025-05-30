6d0baadb35ceecae60ee9d2bce127f7a25888f1d
Joshua Levy <joshua@cal.berkeley.edu>
2025-05-30 15:18:02 -0700

    Add options to disable JS min to avoid bug.

--- src/kash/kits/docs/actions/text/minify_html.py
+++ src/kash/kits/docs/actions/text/minify_html.py
@@ -1,11 +1,17 @@
 from kash.exec import kash_action
 from kash.exec.preconditions import has_fullpage_html_body
-from kash.model import Format, Item
+from kash.model import Format, Item, Param
 from kash.utils.errors import InvalidInput
 
 
-@kash_action(precondition=has_fullpage_html_body)
-def minify_html(item: Item) -> Item:
+@kash_action(
+    precondition=has_fullpage_html_body,
+    params=(
+        Param("no_js_min", "Disable JS minification", bool),
+        Param("no_css_min", "Disable CSS minification", bool),
+    ),
+)
+def minify_html(item: Item, no_js_min: bool = False, no_css_min: bool = False) -> Item:
     """
     Minify an HTML item's content using minify_html, a modern Rust-based minifier.
     """
@@ -16,8 +22,8 @@ def minify_html(item: Item) -> Item:
 
     minified_content = minify(
         item.body,
-        minify_js=True,
-        minify_css=True,
+        minify_js=not no_js_min,
+        minify_css=not no_css_min,
         remove_processing_instructions=True,
         # keep_comments=True,  # Keeps frontmatter format comments.
     )
